bc104f109e9a94898bb77ef7854b11b38e922eee
dnslin <i@dnsl.in>
2024-12-06 00:14:48 +0800

    fix: 修改仓库地址

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 # SQLite数据库URL
 SQLALCHEMY_DATABASE_URL = "sqlite:///./images.db"
